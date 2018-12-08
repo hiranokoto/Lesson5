61ed02e0b70c4b066126207b50ab2129d4ec8620
hiranokoto <udalov.91@mail.ru>
2018-12-08 14:32:28 +0500

    Сделал ДЗ

---
+++ Lesson 5/Проверка ввода логина(Удалов)/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Проверка_ввода_логина_Удалов_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login = "qwerty1234";
+            // Проверка на лишние символы
+            byte[] bytes = new byte[192];
+            int kol = 0;
+            for (int i = 0; i < 47; i++)
+            {
+                bytes[kol] = (byte)i;
+                kol++;
+            }
+            for (int i = 58; i < 64; i++)
+            {
+                bytes[kol] = (byte)i;
+                kol++;
+            }
+            for (int i = 91; i < 96; i++)
+            {
+                bytes[kol] = (byte)i;
+                kol++;
+            }
+            for (int i = 123; i < 255; i++)
+            {
+                bytes[kol] = (byte)i;
+                kol++;
+            }
+
+            char[] chars = System.Text.Encoding.ASCII.GetChars(bytes);
+
+            bool flag = false;
+
+            foreach (var item in chars)
+            {
+                for (int i = 0; i < login.Length; i++)
+                {
+                    if (login[i] == item)
+                    {
+                        flag = true;
+                    }
+                }
+            }
+
+            //
+            if (login.Length >= 2 && login.Length <= 10 && Char.IsDigit((char)login[0]) == false && flag == false)
+                Console.WriteLine("Данный логин - {0}, вполне корректен", login);
+            else
+                Console.WriteLine("Логин не корректен");
+
+        }
+    }
+}
---
+++ Lesson 5/Разраблтать статический класс Message(Удалов)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Разраблтать_статический_класс_Message_Удалов_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Узнал интересный факт о своей солнцезащитной девушке, что бы не плакать пока режешь солнцезащитную цебулю - она надевает солнцезащитные очки. И ей это помогает.";
+            Console.WriteLine(String.Join(" ", text.Split(' ').Where(n => n.Length == text.Split(' ').Max(l => l.Length))));
+            Console.ReadLine();
+        }
+    }
+}
